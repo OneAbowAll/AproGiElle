093e0456c91d1f9a4b1730ade3136f67faaa09cf
OneAbowAll <dado.delgaudio@gmail.com>
2023-03-29 22:35:25 +0200

    Aggiungere i file di progetto.

---
+++ ImparoGiElle/src/Application.cpp
@@ -0,0 +1,228 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include "Renderer.h"
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+#include "IndexBuffer.h"
+#include "VertexArray.h"
+#include "Shader.h"
+#include "Texture.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui_impl_glfw.h"
+
+#include "tests/TestClearColor.h"
+#include "tests/TestTexture2D.h"
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    glfwSwapInterval(1);
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Error!" << std::endl;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    {
+        /*
+        float positions[] = {
+             -50.0f, -50.0f, 0.0f, 0.0f,
+              50.0f, -50.0f, 1.0f, 0.0f,
+              50.0f,  50.0f, 1.0f, 1.0f,
+             -50.0f,  50.0f, 0.0f, 1.0f
+        };
+
+        unsigned int indices[] = {
+            0, 1, 2,
+            2, 3, 0
+        };
+        */
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        /*
+        VertexArray va;
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float)); //4 vertici * 4 floats
+
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
+
+        IndexBuffer ib(indices, 6);
+
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+
+        Shader shader("res/shaders/Basic.shader");
+        shader.Bind();
+
+        shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
+
+        Texture texture("res/textures/Buffo.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0); //Perchè nella riga sopra abbiamo bindato allo slot 0
+
+        va.Unbind();
+        vb.Unbind();
+        ib.Unbind();
+        shader.Unbind();
+        */
+
+        Renderer renderer;
+
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        ImGui::StyleColorsDark();
+
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init((char*)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
+
+        test::Test* currentTest = nullptr;
+        test::TestMenu* testMenu = new test::TestMenu(currentTest);
+        currentTest = testMenu;
+
+        testMenu->RegisterTest<test::TestClearColor>("Clear color");
+        testMenu->RegisterTest<test::TestTexture2D>("Texture 2D");
+
+        /*
+        glm::vec3 translationA(200, 200, 0);
+        glm::vec3 translationB(400, 200, 0);
+
+
+        bool show_demo_window = true;
+        bool show_another_window = false;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+        float r = 0.0f;
+        float increment = 0.05f;
+        */
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            /* Render here */
+            glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            renderer.Clear();
+
+            //test.OnUpdate(0.0f);
+            //test.OnRender();
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+            
+            if (currentTest)
+            {
+                currentTest->OnUpdate(0.0f);
+                currentTest->OnRender();
+
+                ImGui::Begin("Test");
+                if (currentTest != testMenu && ImGui::Button("<-"))
+                {
+                    delete currentTest;
+                    currentTest = testMenu;
+                }
+                currentTest->OnImGuiRender();
+                ImGui::End();
+            }
+
+            //test.OnImGuiRender();
+
+            /*
+            shader.Bind();
+            //shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+                glm::mat4 mvp = proj * view * model;
+                shader.SetUniformMat4f("u_MVP", mvp);
+
+                renderer.Draw(va, ib, shader);
+            }
+
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+                glm::mat4 mvp = proj * view * model;
+                shader.SetUniformMat4f("u_MVP", mvp);
+
+                renderer.Draw(va, ib, shader);
+            }
+
+           // GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+            if (r > 1.0f)
+                increment = -0.05f;
+            else if (r < 0.0f)
+                increment = 0.05f;
+
+            r += increment;
+
+            {
+                ImGui::Begin("Debug");
+
+                ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
+                ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
+
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+                ImGui::End();
+            }
+            */
+
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+
+        delete currentTest;
+
+        if (currentTest != testMenu)
+            delete testMenu;
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwTerminate();
+    return 0;
+}
---
+++ ImparoGiElle/src/tests/TestTexture2D.cpp
@@ -0,0 +1,102 @@
+#include "TestTexture2D.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+#include "Renderer.h"
+#include "imgui/imgui.h"
+
+namespace test {
+
+	
+
+    TestTexture2D::TestTexture2D()
+        :m_Proj(glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f)), m_View(glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0))), 
+        m_TranslationA(200, 200, 0), m_translationB(400, 200, 0)
+    { 
+        float positions[] = {
+            -50.0f, -50.0f, 0.0f, 0.0f,
+             50.0f, -50.0f, 1.0f, 0.0f,
+             50.0f,  50.0f, 1.0f, 1.0f,
+            -50.0f,  50.0f, 0.0f, 1.0f
+        };
+
+        unsigned int indices[] = {
+            0, 1, 2,
+            2, 3, 0
+        };
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        m_Shader = std::make_unique<Shader>("res/shaders/Basic.shader");
+        m_VAO = std::make_unique<VertexArray>();
+
+        m_VertexBuffer = std::make_unique<VertexBuffer>(positions, 4 * 4 * sizeof(float)); //4 vertici * 4 floats
+
+        VertexBufferLayout layout;
+        layout.Push<float>(2);
+        layout.Push<float>(2);
+
+        m_VAO->AddBuffer(*m_VertexBuffer, layout);
+        m_IndexBuffer = std::make_unique<IndexBuffer>(indices, 6);
+
+        m_Proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        m_View = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+
+        m_Shader->Bind();
+
+        m_Texture = std::make_unique<Texture>("res/textures/Buffo.png");
+        m_Shader->SetUniform1i("u_Texture", 0); //Perchè nella riga sopra abbiamo bindato allo slot 0
+    }
+
+    TestTexture2D:: ~TestTexture2D()
+	{
+	}
+
+	void TestTexture2D::OnUpdate(float deltaTime)
+	{
+	}
+
+	void TestTexture2D::OnRender()
+	{
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        Renderer renderer;
+
+        m_Texture->Bind();
+        m_Shader->Bind();
+
+        //shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+
+        {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), m_TranslationA);
+            glm::mat4 mvp = m_Proj * m_View * model;
+            m_Shader->SetUniformMat4f("u_MVP", mvp);
+
+            renderer.Draw(*m_VAO, *m_IndexBuffer, *m_Shader);
+        }
+
+        {
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), m_translationB);
+            glm::mat4 mvp = m_Proj * m_View * model;
+            m_Shader->SetUniformMat4f("u_MVP", mvp);
+
+            renderer.Draw(*m_VAO, *m_IndexBuffer, *m_Shader);
+        }
+        
+	}
+
+	void TestTexture2D::OnImGuiRender()
+	{
+        ImGuiIO io = ImGui::GetIO();
+        
+        ImGui::SliderFloat3("Translation A", &m_TranslationA.x, 0.0f, 960.0f);
+        ImGui::SliderFloat3("Translation B", &m_translationB.x, 0.0f, 960.0f);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+
+    }
+
+}
